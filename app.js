3113cc4c4511c6e26bd837e2c8f097b214e3388f
MartinSierraCardenas <martin.sierra.mt@usa.edu.co>
2022-09-15 21:46:00 -0500

    //mi primera version 1.0

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+var time= new Date();
+alert(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+//alert("esto es un mensaje en una ventana!con alert");
+document.write("la informacion se muestra dentro de la pagina web html con document.write");
+//document.write("<h3>mi segundo parrafo</h3>");
+
+var num1 =1;
+var num2=2;
+alert(num1+num2);
+
+
+//otras maneras de crear los scripts es directamente en el codigo html
+
+//en el Head y en el body
+
+// en el head:
+//var time = new Date();
+//console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+
+// en el body
+//let d = new Date();
+//document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + //d.getMinutes() + ":" + d.getSeconds()
+
+
+//diferencias entre let, var, y const 
+//https://cybmeta.com/var-let-y-const-en-javascript
+
+
+	tting_started_with_the_web/JavaScript_basics
